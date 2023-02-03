ed546b5fd04318cac6b137b8e937562c32e9c333
Fabio Hernan Gomez Rubio <fabiogomez11c@gmail.com>
2023-02-02 20:13:23 -0500

    last update

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from fastapi import FastAPI
+import os
+
+from google.cloud import storage
 import tensorflow as tf
+from flask import Flask
 
-app = FastAPI()
+app = Flask(__name__)
 
-model1 = tf.keras.models.load_model('model.h5')
-model2 = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model("gs://mom_seguros_images_car/model_output/ic_v4.7")
 
-@app.get("/")
-def read_root():
-  return {"Hello": "World"}
+@app.route("/")
+def hello_world():
+    name = os.environ.get("NAME", "World")
+    return "Hello {}!".format(name)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
